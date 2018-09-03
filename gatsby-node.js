b178907f26f8b07aae29359607ed6d82846f1b9c
benjamin hoffman <6520022+benjaminhoffman@users.noreply.github.com>
2018-09-03 16:45:37 -0700

    Throws an error if mailchimp endpoint is incorrect

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,7 +11,10 @@ exports.modifyWebpackConfig = function (_ref, _ref2) {
   var isString = typeof endpoint === 'string';
   if (!isString) {
     throw 'Mailchimp endpoint required and must be of type string. See repo README for more info.';
+  } else if (endpoint.length < 40) {
+    throw 'gatsby-plugin-mailchimp: donʼt forget to add your MC endpoint to your gatsby-config file. See README for more info.';
   }
+
   config.plugin('Mailchimp', DefinePlugin, [{ __GATSBY_PLUGIN_MAILCHIMP_ADDRESS__: JSON.stringify(endpoint) }]);
   return config;
 };
--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -4,7 +4,10 @@ exports.modifyWebpackConfig = ({ config, stage }, { endpoint }) => {
   const isString = typeof endpoint === 'string'
   if (!isString) {
     throw `Mailchimp endpoint required and must be of type string. See repo README for more info.`
+  } else if (endpoint.length < 40) {
+    throw 'gatsby-plugin-mailchimp: donʼt forget to add your MC endpoint to your gatsby-config file. See README for more info.'
   }
+
   config.plugin('Mailchimp', DefinePlugin, [
     { __GATSBY_PLUGIN_MAILCHIMP_ADDRESS__: JSON.stringify(endpoint) }
   ]);
