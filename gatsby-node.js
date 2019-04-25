9c834761dae092c6c17b08e4a6b37eaa9dde60b4
benjamin hoffman <6520022+benjaminhoffman@users.noreply.github.com>
2019-04-25 16:52:42 -0700

    Adds ESLint functionality

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,20 @@
 'use strict';
 
 exports.onCreateWebpackConfig = function (_ref, _ref2) {
-  var plugins = _ref.plugins,
-      actions = _ref.actions;
-  var endpoint = _ref2.endpoint;
+    var plugins = _ref.plugins,
+        actions = _ref.actions;
+    var endpoint = _ref2.endpoint;
 
-  var isString = typeof endpoint === 'string';
-  if (!isString) {
-    throw 'Mailchimp endpoint required and must be of type string. See repo README for more info.';
-  } else if (endpoint.length < 40) {
-    throw 'gatsby-plugin-mailchimp: donʼt forget to add your MC endpoint to your gatsby-config file. See README for more info.';
-  }
+    var isString = typeof endpoint === 'string';
+    if (!isString) {
+        throw 'Mailchimp endpoint required and must be of type string. See repo README for more info.';
+    } else if (endpoint.length < 40) {
+        throw 'gatsby-plugin-mailchimp: donʼt forget to add your MC endpoint to your gatsby-config file. See README for more info.';
+    }
 
-  actions.setWebpackConfig({
-    plugins: [plugins.define({
-      __GATSBY_PLUGIN_MAILCHIMP_ADDRESS__: JSON.stringify(endpoint)
-    })]
-  });
+    actions.setWebpackConfig({
+        plugins: [plugins.define({
+            __GATSBY_PLUGIN_MAILCHIMP_ADDRESS__: JSON.stringify(endpoint)
+        })]
+    });
 };
